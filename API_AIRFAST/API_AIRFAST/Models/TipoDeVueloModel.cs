f00e5cd96af8d049287a9b29a75147916eb52c2b
dslopez0618@gmail.com <dslopez0618@gmail.com>
2024-11-01 08:41:03 -0500

    modelos de vuelo y tipo de vuelo

---
+++ API_AIRFAST/API_AIRFAST/Models/TipoDeVueloModel.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace API_AIRFAST.Models;
+
+public class TipoDeVueloModel
+{
+    /// <summary>
+    /// ID DEL TIPO DE VUELO.
+    /// </summary>
+    [Key]
+    [Column("id")]
+    public int Id { get; set; }
+
+    /// <summary>
+    /// DESCRIPCION DEL TIPO DE VUELO (NACIONAL O INTERNACIONAL).
+    /// </summary>
+    [Column("descripcion")]
+    public string Descripcion { get; set; }
+}
---
+++ API_AIRFAST/API_AIRFAST/Models/VueloModel.cs
@@ -0,0 +1,75 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace API_AIRFAST.Models;
+
+public class VueloModel
+{
+    /// <summary>
+    /// ID DEL VUELO AUTOGENERADO.
+    /// </summary>
+    [Key]
+    [Column("id")]
+    public int Id { get; set; }
+
+    /// <summary>
+    /// FECHA DE SALIDA DEL VUELO.
+    /// </summary>
+    [Column("fecha_vuelo")]
+    public DateTime FechaVuelo { get; set; }
+
+    /// <summary>
+    /// HORA DE SALIDA DEL VUELO.
+    /// </summary>
+    [Column("hora_vuelo")]
+    public TimeSpan HoraVuelo { get; set; }
+
+    /// <summary>
+    /// CIUDAD DE ORIGEN DEL VUELO.
+    /// </summary>
+    [Column("origen")]
+    public string Origen { get; set; }
+
+    /// <summary>
+    /// CIUDAD DE DESTINO DEL VUELO.
+    /// </summary>
+    [Column("destino")]
+    public string Destino { get; set; }
+
+    /// <summary>
+    /// TIEMPO ESTIMADO DE DURACION DEL VUELO.
+    /// </summary>
+    [Column("tiempo_de_vuelo")]
+    public TimeSpan TiempoDeVuelo { get; set; }
+
+    /// <summary>
+    /// ID DEL TIPO DE VUELO (NACIONAL O INTERNACIONAL).
+    /// </summary>
+    [Column("id_tipo_vuelo")]
+    public int IdTipoVuelo { get; set; }
+
+    /// <summary>
+    /// OBJETO DE NAVEGACION PARA EL TIPO DE VUELO.
+    /// </summary>
+    public TipoDeVueloModel TipoDeVuelo { get; set; }
+
+    /// <summary>
+    /// FECHA DE LLEGADA DEL VUELO.
+    /// </summary>
+    [Column("fecha_llegada")]
+    public DateTime FechaLlegada { get; set; }
+
+    /// <summary>
+    /// HORA DE LLEGADA DEL VUELO.
+    /// </summary>
+    [Column("hora_llegada")]
+    public TimeSpan HoraLlegada { get; set; }
+
+    /// <summary>
+    /// COSTO DEL VUELO POR PERSONA.
+    /// </summary>
+    [Column("costo_por_persona")]
+    public decimal CostoPorPersona { get; set; }
+
+}
+
